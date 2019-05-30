f127dd6102dbb31d2b26acc502089c8075a16a2e
martin0499 <48216671+martin0499@users.noreply.github.com>
2019-05-30 17:48:31 +0300

    Add files via upload

---
+++ Zadacha_12/Zadacha_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word, reverse = "";
+            Console.Write("Въведете дума: ");
+            word = Console.ReadLine();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverse += word[i].ToString();
+            }
+            if (reverse == word)
+            {
+                Console.Write("Думата е палиндром. Въведената дума е {0} и обратното е {1}", word, reverse);
+            }
+            else
+            {
+                Console.Write("Думата не е палиндром. Въведената дума е {0} и обратното е {1}", word, reverse);
+            }
+            Console.ReadKey();
+        }
+    }
+}
